9b2ca4b5a74bf3187654ee65cfe17ff82cfa2d03
Andrew <starandry83@mail.ru>
2024-11-13 12:13:54 +0300

    settings are styled

--- src/components/containers/Background/index.tsx
+++ src/components/containers/Background/index.tsx
@@ -1,12 +1,23 @@
 import React, { ReactNode } from 'react';
 import styles from './background.module.scss';
+import {useLocation} from "react-router-dom";
 
 type BackgroundProps = {
     children: ReactNode;
 };
 
 const Background: React.FC<BackgroundProps> = ({ children }) => {
-    return <div className={styles.background}>{children}</div>;
+    const location = useLocation();
+    const currentPath = location.pathname;
+    let compBackground;
+
+    if(currentPath === '/settings') {
+        compBackground = `${styles.background} ${styles.backgroundSettings}`;
+    } else {
+        compBackground = styles.background;
+    }
+
+    return <div className={compBackground}>{children}</div>;
 };
 
 export { Background };
