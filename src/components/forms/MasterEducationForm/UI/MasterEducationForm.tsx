78df1660847bdc4c1395109089159c421612d993
Andrew <starandry83@mail.ru>
2025-12-01 22:14:54 +0300

    temp commit starting master education form

--- src/components/forms/MasterEducationForm/UI/MasterEducationForm.tsx
+++ src/components/forms/MasterEducationForm/UI/MasterEducationForm.tsx
@@ -1,8 +1,41 @@
 import React from 'react';
-/*import styles from './index.module.scss';*/
+import styles from './index.module.scss';
+import { Input } from '@/components';
 
 const MasterEducationForm: React.FC = () => {
-    return <div></div>;
+    return (
+        <div className={styles.educationOverlay}>
+            <form className={styles.educationForm}>
+                <h2 className={styles.educationFormTitle}>Редактировать поле Образование</h2>
+                <div className={styles.educationFieldWrapper}>
+                    <div className={styles.institutionGroup}>
+                        <label className={styles.institutionLabel}>
+                            Название учебного заведения
+                        </label>
+
+                        <Input
+                            type='text'
+                            placeholder='Введите название учебного заведения'
+                            className='institutionInput'
+                            onChange={() => {}}
+                        />
+                    </div>
+                    <div className={styles.institutionGroup}>
+                        <label className={styles.institutionLabel}>
+                            Название учебного заведения
+                        </label>
+
+                        <Input
+                            type='text'
+                            placeholder='Введите название учебного заведения'
+                            className='institutionInput'
+                            onChange={() => {}}
+                        />
+                    </div>
+                </div>
+            </form>
+        </div>
+    );
 };
 
 export { MasterEducationForm };
