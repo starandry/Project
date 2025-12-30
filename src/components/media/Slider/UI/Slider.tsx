250dcd9ab292d47a9f265260813c585a9458dbd1
Andrew <starandry83@mail.ru>
2025-12-30 21:38:44 +0300

    fix slider

--- src/components/media/Slider/UI/Slider.tsx
+++ src/components/media/Slider/UI/Slider.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
 import 'swiper/css';
+import 'swiper/css/pagination';
 import type { SliderProps } from '@/components/media/Slider/index.model.ts';
 import { DiscountCard } from '@/components';
 import styles from './index.module.scss';
 import './slider.css';
 
 const Slider: React.FC<SliderProps> = ({
-    slides = [1, 2, 3, 4, 5, 6],
+    slides = [1, 2, 3, 4, 5],
     discountPercent = 20,
     spaceBetween = 8,
 }) => {
     return (
         <div className={styles.sliderWrap}>
             <Swiper
-                slidesPerView={3.25}
+                modules={[Pagination]}
+                pagination={{ clickable: true }}
+                slidesPerView={3}
+                slidesPerGroup={1}
                 spaceBetween={spaceBetween}
                 breakpoints={{
                     320: { slidesPerView: 1 },
                     640: { slidesPerView: 1.5 },
                     768: { slidesPerView: 2 },
-                    1024: { slidesPerView: 3.25 },
+                    1024: { slidesPerView: 3 },
                 }}
                 className={styles.swiper}
             >
