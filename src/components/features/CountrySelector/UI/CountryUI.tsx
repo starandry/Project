049fc44a49ceeddd969d9dc33911082577d1a59c
Andrew <starandry83@mail.ru>
2026-01-07 21:34:26 +0300

    fix hook

--- src/components/features/CountrySelector/UI/CountryUI.tsx
+++ src/components/features/CountrySelector/UI/CountryUI.tsx
@@ -2,7 +2,7 @@ import React, { RefObject } from 'react';
 import styles from './index.module.scss';
 import { SvgIcon } from '@/components';
 import PurpleArrowDown from '@/assets/icons/PurpleArrowDown.svg?react';
-import { Country } from '../model/countryTypes';
+import type { Country } from '@/hooks/ui';
 
 type Props = {
     wrapperRef: RefObject<HTMLDivElement>;
--- src/hooks/ui/index.ts
+++ src/hooks/ui/index.ts
@@ -1,3 +1,4 @@
 export { useDropdown } from './useDropdown';
 export { useDropdownContainer } from './useDropdownContainer';
 export { useCountrySelector } from './useCountrySelector';
+export type { Country } from './countryTypes';
